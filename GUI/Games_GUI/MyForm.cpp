0a500e7a5afb4e43adef719cbfd450aa6fd4e362
Eslam-Sayed7 <eslam.sayed.ed@gmail.com>
2023-12-10 16:33:52 +0200

    unifing project files

---
+++ GUI/Games_GUI/MyForm.cpp
@@ -0,0 +1,20 @@
+#include "MyForm.h"
+#include"Connect_four_game.h"
+#include"Five_Five_Board.h"
+#include"Pyramic_Board.h"
+#include <Windows.h>
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+int main(array<System::String^>^ args)
+{
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // Create and run GameOptionsForm
+    Application::Run(gcnew GamesGUI::GameOptionsForm());
+
+    return 0;
+}
