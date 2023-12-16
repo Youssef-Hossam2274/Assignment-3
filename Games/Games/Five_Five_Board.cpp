b49945ad80f153632327c8cc0c5364e3c8f13ffb
abdullah saeed alnami <bdallhsydalnmy@gmail.com>
2023-12-16 22:00:01 +0200

    Update Five_Five_Board.cpp

--- Games/Games/Five_Five_Board.cpp
+++ Games/Games/Five_Five_Board.cpp
@@ -13,6 +13,15 @@ Five_Five_Board::Five_Five_Board() {
     }
 }
 
+bool Five_Five_Board::update_board(int x, int y, char mark) {
+    if (x < n_rows && y < n_cols && board[x][y] == 0) {
+        board[x][y] = toupper(mark);
+        n_moves++;
+        return true;
+    }
+    return false;
+}
+
 int Five_Five_Board::check_status() {
     if (n_moves != 24)
         return 1;
@@ -46,23 +55,50 @@ int Five_Five_Board::check_status() {
         return (it2->first == 'X') ? 2 : -2;
 }
 
-bool Five_Five_Board::update_board(int x, int y, char mark) {
-    if (x < n_rows && y < n_cols && board[x][y] == 0) {
-        board[x][y] = toupper(mark);
-        n_moves++;
-        return true;
+bool Five_Five_Board::is_winner() {
+    if (n_moves != 24)
+        return false;
+    map<char, int> symbol_points;
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            if (board[i][j] != 0 && i <=2 && j <= 2) {
+                if (board[i][j] == board[i][j + 1] && board[i][j + 1] == board[i][j + 2])
+                    symbol_points[board[i][j]]++;
+
+                if (board[i][j] == board[i + 1][j] && board[i + 1][j] == board[i + 2][j])
+                    symbol_points[board[i][j]]++;
+
+
+                if (board[i][j] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j + 2])
+                    symbol_points[board[i + 1][j + 1]]++;
+
+                if (board[i][j + 2] == board[i + 1][j + 1] && board[i + 1][j + 1] == board[i + 2][j])
+                    symbol_points[board[i + 1][j + 1]]++;
+            }
+        }
     }
+    auto it1 = symbol_points.begin();
+    auto it2 = ++symbol_points.begin();
+    if (it1->second == it2->second)
+        cout << "Draw!\n";
+    else if (it1->second > it2->second)
+        cout << "player with symbol " << it1->first << " wins with " << it1->second<<" points\n";
+
+    else
+        cout << "player with symbol " << it2->first << " wins with " << it2->second<<" points\n";
     return false;
 }
 
-bool Five_Five_Board::is_winner()
-{
-    int res = check_status();
-    if (res == 2 || res == -2)
+bool Five_Five_Board::is_draw() {
+    return false;
+}
+
+bool Five_Five_Board::game_is_over() {
+    if (n_moves == 24)
         return true;
-
     return false;
 }
+
 
 void Five_Five_Board::display_board() {
     cout << '\n';
@@ -78,15 +114,7 @@ void Five_Five_Board::display_board() {
     }
 }
 
-bool Five_Five_Board::is_draw() {
-    return (check_status() == 0);
-}
 
-bool Five_Five_Board::game_is_over() {
-    if (n_moves == 24)
-        return true;
-    return false;
-}
 
 int Five_Five_Board::minimax(int &x, int &y, bool isMaximizing, bool firstTime) {
     return 0;
